09a88d5b2d1e03bfbb17920b389fd857754e7585
Kajen_Duvall <kajen.duvall@gmail.com>
2024-04-07 20:35:33 -0600

    Caesar Cipher Assignment

--- cipher.py
+++ cipher.py
@@ -1,1 +1,22 @@
-# add your code here
+# Prompt user to enter a sentence
+print("Please enter a sentence: ")
+sentence = input()     
+
+print("You entered: " + sentence)
+
+# Create string for indexing
+alphabet="abcdefghijklmnopqrstuvwxyz"
+encrypted_sentence= ""
+
+# Set up shifting
+for i in range(0, len(sentence)):
+  try:
+    char=sentence[i]
+    encrypted_char=str(alphabet[alphabet.index(char)+5])
+    encrypted_sentence+=encrypted_char
+  except:
+    encrypted_char=str(alphabet[0])
+    encrypted_sentence+=encrypted_char
+
+# Print Caesar Cipher
+print("The encrypted sentence is: "+ encrypted_sentence)
